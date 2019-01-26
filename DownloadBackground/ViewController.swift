86e4036c7ce2c351915c746a16e65a5ce578cb2d
Tiago Martinho <t.martinho@icloud.com>
2019-01-26 12:03:08 +0100

    Refactor

--- DownloadBackground/ViewController.swift
+++ DownloadBackground/ViewController.swift
@@ -22,28 +22,12 @@ class ViewController: UIViewController {
         session = URLSession(configuration: config, delegate: self, delegateQueue: nil)
     }
 
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        updateUI()
-    }
-
-    func updateUI() {
-        let documentsURL = try?
-            FileManager.default.url(for: .documentDirectory,
-                                    in: .userDomainMask,
-                                    appropriateFor: nil,
-                                    create: false)
-        let path = documentsURL?.path ?? ""
-        let content = try? FileManager.default.contentsOfDirectory(atPath: path)
-        downloadedFiles.text = content?.reduce("") { "\($0)\($1)" }
-    }
-
     @IBAction func startDownload(_ sender: Any) {
         if let data = self.resumeData {
             backgroundTask = session?.downloadTask(withResumeData: data)
         } else {
-            let  url = URL(string: "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4")!
-            backgroundTask = session?.downloadTask(with: url)
+            let endpoint = "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4"
+            backgroundTask = session?.downloadTask(with: URL(string: endpoint)!)
         }
         backgroundTask?.resume()
     }
@@ -52,38 +36,14 @@ class ViewController: UIViewController {
 extension ViewController: URLSessionDelegate, URLSessionDownloadDelegate {
 
     func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
-        guard let error = error else {
-            // Handle success case.
-            return
-        }
+        guard let error = error else { return }
         let userInfo = (error as NSError).userInfo
         if let resumeData = userInfo[NSURLSessionDownloadTaskResumeData] as? Data {
             self.resumeData = resumeData
         }
-        // Perform any other error handling.
-    }
-
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        print("didFinishDownloadingTo")
-        do {
-            let documentsURL = try
-                FileManager.default.url(for: .documentDirectory,
-                                        in: .userDomainMask,
-                                        appropriateFor: nil,
-                                        create: false)
-            let savedURL = documentsURL.appendingPathComponent(
-                location.lastPathComponent)
-            try FileManager.default.moveItem(at: location, to: savedURL)
-            DispatchQueue.main.async {
-                self.updateUI()
-            }
-        } catch {
-            // handle filesystem error
-        }
     }
 
     func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {
-        print("urlSessionDidFinishEvents")
         DispatchQueue.main.async {
             guard let appDelegate = UIApplication.shared.delegate as? AppDelegate,
                 let backgroundCompletionHandler =
@@ -104,12 +64,12 @@ extension ViewController: URLSessionDelegate, URLSessionDownloadDelegate {
             DispatchQueue.main.async {
                 print(NSNumber(value: calculatedProgress))
                 self.progressLabel.text = "\(calculatedProgress)"
-                self.progressView.progress = Float(NSNumber(value: calculatedProgress))
+                self.progressView.progress = Float(truncating: NSNumber(value: calculatedProgress))
             }
         }
     }
 
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didResumeAtOffset fileOffset: Int64, expectedTotalBytes: Int64) {
-        print("download resumed")
-    }
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didResumeAtOffset fileOffset: Int64, expectedTotalBytes: Int64) { }
+
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) { }
 }
