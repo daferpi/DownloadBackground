0f479a8a8daf024d9be15efaa7dda2392028a85f
Tiago Martinho <t.martinho@icloud.com>
2019-01-26 11:16:37 +0100

    Download files in foreground

--- DownloadBackground/ViewController.swift
+++ DownloadBackground/ViewController.swift
@@ -1,14 +1,53 @@
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
+    private var backgroundTask: URLSessionDownloadTask?
+
+    private lazy var session: URLSession = {
+        let config = URLSessionConfiguration.background(withIdentifier: "MySession")
+        config.isDiscretionary = false
+        config.sessionSendsLaunchEvents = true
+        return URLSession(configuration: config, delegate: self, delegateQueue: nil)
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let  url = URL(string: "https://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_320x180.mp4")!
+        backgroundTask = session.downloadTask(with: url)
+        backgroundTask?.resume()
+        print("backgroundTask.resume")
     }
-
-
 }
 
+extension ViewController: URLSessionDelegate, URLSessionDownloadDelegate {
+
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        print("didFinishDownloadingTo")
+    }
+
+    func urlSessionDidFinishEvents(forBackgroundURLSession session: URLSession) {
+        print("urlSessionDidFinishEvents")
+        DispatchQueue.main.async {
+            guard let appDelegate = UIApplication.shared.delegate as? AppDelegate,
+                let backgroundCompletionHandler =
+                appDelegate.backgroundCompletionHandler else {
+                    return
+            }
+            backgroundCompletionHandler()
+        }
+    }
+
+    func urlSession(_ session: URLSession,
+                    downloadTask: URLSessionDownloadTask,
+                    didWriteData bytesWritten: Int64,
+                    totalBytesWritten: Int64,
+                    totalBytesExpectedToWrite: Int64) {
+        if downloadTask == self.backgroundTask {
+            let calculatedProgress = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
+            DispatchQueue.main.async {
+                print(NSNumber(value: calculatedProgress))
+            }
+        }
+    }
+}
